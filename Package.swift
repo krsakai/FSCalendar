b5792359ff8fa8e62eac1563639760c4b0cfb887
酒井邦也 <k_sakai@waku-2.com>
2020-03-15 05:25:24 +0900

    SwiftPM

--- Package.swift
+++ Package.swift
@@ -11,9 +11,6 @@ let package = Package(
             name: "FSCalendar",
             targets: ["FSCalendar"]),
     ],
-    dependencies: [
-        .package(url: "https://github.com/krsakai/FSCalendar", .branch("master"))
-    ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
